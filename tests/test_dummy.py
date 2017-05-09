4e6e729e92bd3c092dbe1ebb82ce4d3abbc55616
Dawid Tomasiewicz <datom2912@interia.pl>
2017-05-09 16:59:34 +0200

    Add tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -10,11 +10,29 @@ Run with PYTHONPATH=. python tests/test_dummy.py
 class TestDummy(unittest.TestCase):
 
     def test_fun(self):
-        weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        weekday = calculate(2004, 2, 29)
+        self.assertEqual(weekday, 7)
+
+        with self.assertRaises(Exception):
+            calculate(1900, 2, 29)
+        with self.assertRaises(Exception):
+            calculate(178, 255, 29)
+        with self.assertRaises(Exception):
+            calculate(1900, 2, 293)
+        with self.assertRaises(Exception):
+            calculate(2017, 32, 0)
+
+        weekday = calculate(1791, 5, 3)
+        self.assertEqual(weekday, 2)
+
+
+
 
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
+
+        with self.assertRaises(Exception):
+            main(("--year", 555555, "--month", 5))
 
 
 if __name__ == '__main__':
